95a7168cf8a63d4b734f683a64c7e42af7516602
Julineo <advoretskiy80@gmail.com>
2018-06-20 11:44:27 -0700

    solved

---
+++ intro/extractEachKth/main.go
@@ -0,0 +1,18 @@
+/*
+https://codefights.com/arcade/intro/level-8/3AgqcKrxbwFhd3Z3R
+Given array of integers, remove each kth element from it.
+
+Example
+
+For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3, the output should be
+extractEachKth(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].
+*/
+func extractEachKth(inputArray []int, k int) []int {
+    res := []int{}
+    for i := range inputArray {
+        if (i + 1) % k != 0 {
+            res = append(res, inputArray[i])
+        }
+    }
+    return res
+}
