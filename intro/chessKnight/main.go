f0f0e50d93a304db3f38af85526b06c1a8ae7b98
Julineo <advoretskiy80@gmail.com>
2018-08-28 11:50:34 -0700

    solved

---
+++ intro/chessKnight/main.go
@@ -0,0 +1,29 @@
+/*
+https://app.codesignal.com/arcade/intro/level-11/pwRLrkrNpnsbgMndb
+
+Given a position of a knight on the standard chessboard, find the number of different moves the knight can perform.
+
+The knight can move to a square that is two squares horizontally and one square vertically, or two squares vertically and one square horizontally away from it. The complete move therefore looks like the letter L. Check out the image below to see all valid moves for a knight piece that is placed on one of the central squares.
+
+
+Example
+
+For cell = "a1", the output should be
+chessKnight(cell) = 2.
+
+For cell = "c2", the output should be
+chessKnight(cell) = 6.
+*/
+func chessKnight(cell string) (r int) {
+    x := cell[0] - 96
+    y := cell[1] - 48
+        if x + 1 > 0 && y + 2 > 0 && x + 1 < 9 && y + 2 < 9 {r++}
+        if x + 2 > 0 && y + 1 > 0 && x + 2 < 9 && y + 1 < 9 {r++}
+        if x + 2 > 0 && y - 1 > 0 && x + 2 < 9 && y - 1 < 9 {r++}
+        if x + 1 > 0 && y - 2 > 0 && x + 1 < 9 && y - 2 < 9 {r++}
+        if x - 1 > 0 && y - 2 > 0 && x - 1 < 9 && y - 2 < 9 {r++}
+        if x - 2 > 0 && y - 1 > 0 && x - 2 < 9 && y - 1 < 9 {r++}
+        if x - 2 > 0 && y + 1 > 0 && x - 2 < 9 && y + 1 < 9 {r++}
+        if x - 1 > 0 && y + 2 > 0 && x - 1 < 9 && y + 2 < 9 {r++}
+    return
+}
