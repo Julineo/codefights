2032e310d85d31f844db482b8364ecf77f190e05
Julineo <advoretskiy80@gmail.com>
2018-09-10 14:18:02 -0700

    solved

---
+++ databases/hostnamesOrdering.sql
@@ -0,0 +1,43 @@
+/*
+https://app.codesignal.com/arcade/db/selecting-what-to-select/rpo4gmB8H8tpm5TEJ
+
+Yesterday you wrote down the links for some resources that you are going to use as references in your academic paper, and now you want to sort them. You didn't bother to write down the entire link for any of them, so all you have is a bunch of unique hostnames.
+
+You stored the information about these hostnames in the table hostnames, which has the structure:
+
+id: the unique hostname id;
+hostname: the unique hostname.
+Each hostname contain several domains and can be written in the format hostname = domain1.domain2. .... To sort the hostnames, you've decided that hostname A should go before hostname B in the sorted list if its reversed list of domains is lexicographically smaller than the reversed list of B's domains.
+
+Given the table hostnames, build the resulting table as follows: The table should have columns id and hostname, and its values should be ordered as described above. It is guaranteed that all the hostnames are different and that there are no more than 3 domains in each hostname.
+
+Example
+
+For the following table hostnames
+
+id	hostname
+1	workbench.mysql.com
+2	codesignal.slack.com
+3	codesignal.com
+4	snarknews.info
+5	sololearn.com
+6	dev.mysql.com
+
+the output should be
+
+id	hostname
+3	codesignal.com
+6	dev.mysql.com
+1	workbench.mysql.com
+2	codesignal.slack.com
+5	sololearn.com
+4	snarknews.info
+*/
+CREATE PROCEDURE hostnamesOrdering()
+BEGIN
+    SELECT id, hostname
+    FROM hostnames
+    GROUP BY CONCAT(SUBSTRING_INDEX(CONCAT('...',hostname),'.',-1),
+               ' ', SUBSTRING_INDEX(CONCAT('...',hostname),'.',-2),
+               ' ', hostname);
+END
