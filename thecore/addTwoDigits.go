9ae0575b4a53ad795d6e439e0ebbaceb4a615be1
Julineo <advoretskiy80@gmail.com>
2018-09-19 11:41:07 -0700

    solved add two digits

---
+++ thecore/addTwoDigits.go
@@ -0,0 +1,9 @@
+/*
+https://app.codesignal.com/arcade/code-arcade/intro-gates/wAGdN6FMPkx7WBq66
+
+You are given a two-digit integer n. Return the sum of its digits.
+*/
+
+func addTwoDigits(n int) int {
+    return (n / 10) + (n % 10)
+}
