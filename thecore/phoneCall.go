363ee0f8ce491585fc732b0ad19b1885eeaec024
Julineo <advoretskiy80@gmail.com>
2018-09-26 11:49:13 -0700

    solved

---
+++ thecore/phoneCall.go
@@ -0,0 +1,39 @@
+/*
+https://app.codesignal.com/arcade/code-arcade/intro-gates/mZAucMXhNMmT7JWta
+
+Some phone usage rate may be described as follows:
+
+first minute of a call costs min1 cents,
+each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
+each minute after 10th costs min11 cents.
+You have s cents on your account before the call. What is the duration of the longest call (in minutes rounded down to the nearest integer) you can have?
+
+Example
+
+For min1 = 3, min2_10 = 1, min11 = 2, and s = 20, the output should be
+phoneCall(min1, min2_10, min11, s) = 14.
+
+Here's why:
+
+the first minute costs 3 cents, which leaves you with 20 - 3 = 17 cents;
+the total cost of minutes 2 through 10 is 1 * 9 = 9, so you can talk 9 more minutes and still have 17 - 9 = 8 cents;
+each next minute costs 2 cents, which means that you can talk 8 / 2 = 4 more minutes.
+Thus, the longest call you can make is 1 + 9 + 4 = 14 minutes long.
+*/
+func phoneCall(min1 int, min2_10 int, min11 int, s int) (res int) {
+    s = s - min1
+    if s < 0 { return }
+    res = 1
+    if s <= 0 { return }
+    for i := 2; i <= 10; i++ {
+        s = s - min2_10
+        if s < 0 { return }
+        res++
+    }
+    for {
+        s = s - min11
+        if s < 0 { return }
+        res++
+    }
+    return
+}
