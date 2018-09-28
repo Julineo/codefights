8362ffe95e9c93220567c0b88705b23a98344b13
Julineo <advoretskiy80@gmail.com>
2018-09-28 16:12:53 -0700

    solved extra number

---
+++ thecore/extraNumber.go
@@ -0,0 +1,17 @@
+/*
+https://app.codesignal.com/arcade/code-arcade/at-the-crossroads/sgDWKCFQHHi5D3Szj
+
+You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?
+
+Example
+
+For a = 2, b = 7, and c = 2, the output should be
+extraNumber(a, b, c) = 7.
+
+The two equal numbers are a and c. The third number (b) equals 7, which is the answer.
+*/
+
+func extraNumber(a int, b int, c int) int {
+    fmt.Println(1 ^ 5)
+    return 0
+}
