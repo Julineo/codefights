6b2f8f2434f7a5ea8158c44ee6e015a897f8cf10
Julineo <advoretskiy80@gmail.com>
2018-10-23 16:25:55 -0700

    solved tastks

---
+++ python/repeatChar.py
@@ -0,0 +1,11 @@
+"""
+https://app.codesignal.com/arcade/python-arcade/lambda-illusions/bCAsRfNJrjD9zhxkS
+
+Implement a function that, given a character ch and the number of times it should be repeated n, returns a string of n characters ch.
+
+Example
+
+For ch = '*' and n = 20, the output should be
+repeatChar(ch, n) = "********************".
+"""
+repeatChar = lambda c, n: c * n
